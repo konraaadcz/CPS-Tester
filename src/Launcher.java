56d94d2f2a5088c6bb0b72d7204c2f52e2ec4d0a
Konraaaaaaaad <czmblkonrad@gmail.com>
2025-04-03 21:14:30 +0200

    Update Launcher.java
    implementing loadingscreen

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,7 +5,13 @@ import frame.*;
 public class Launcher {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args ) throws InterruptedException  {
+
+        LoadingScreen a = new LoadingScreen();
+        a.showLoadingScreen();
+        Thread.sleep(5000);
+
+        a.closeLoadingScreen();
         SwingUtilities.invokeLater(Frame::new);
     }
 
